5eda18bb77ae7212d359fca17da371fe8e8a4e88
Siau Tan Long <stl7@hotmail.com>
2019-07-31 22:55:01 +0800

    Create 03-demo-grep-mapreduce.sh

---
+++ 03-demo-grep-mapreduce.sh
@@ -0,0 +1,8 @@
+hadoop jar /opt/cloudera/parcels/CDH-5.10.0-1.cdh5.10.0.p0.41/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar grep hdfs://smartconnectzonename:8020/backup/enwiki-latest-all-titles.gz hdfs://cdhdaszone.isilon.com:8020/output 'dell'
+echo ""
+
+echo "hdfs dfs -ls -h hdfs://smartconnectzonename:8020/output"
+hdfs dfs -ls -h hdfs://smartconnectzonename:8020/output
+
+echo "hdfs dfs -cat hdfs://smartconnectzonename:8020/output/part-r-00000"
+hdfs dfs -cat hdfs://smartconnectzonename:8020/output/part-r-00000
